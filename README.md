d8add089d0ec3990604b2c42dbaf0750a7485ae9
aktos electronics <aea@node>
2017-09-12 19:23:47 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# `create-gist.sh` 
+
+A command line utility to create gist from command line
+
+
+        usage: 
+
+                create-gist.sh /path/to/file [Github user name]
+
+
---
+++ create-gist.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# based on: https://gist.github.com/s-leroux/7cb7424d33ba3753e907cc2553bcd1ba
+# modified by: cca
+
+
+print_usage(){
+  cat << USAGE
+
+	usage: 
+		
+		$(basename $0) /path/to/file [Github user name]
+
+USAGE
+}
+
+# 0. Your file name
+FNAME=$1
+
+if [[ ! -f $FNAME ]]; then
+  print_usage
+  exit 2
+fi
+
+# 0.1 Your username (optional)
+GITHUB_USERNAME=$2
+
+
+# 1. Somehow sanitize the file content
+#    Remove \r (from Windows end-of-lines),
+#    Replace tabs by \t
+#    Replace " by \"
+#    Replace EOL by \n
+CONTENT=$(sed -e 's/\r//' -e's/\t/\\t/g' -e 's/"/\\"/g' "${FNAME}" | awk '{ printf($0 "\\n") }')
+
+# 2. Build the JSON request
+read -r -d '' DESC <<EOF
+{
+  "description": "some description",
+  "public": true,
+  "files": {
+    "${FNAME}": {
+      "content": "${CONTENT}"
+    }
+  }
+}
+EOF
+
+# 3. Use curl to send a POST request
+
+if [[ "$GITHUB_USERNAME" != "" ]]; then
+  # REGISTERED USER
+  curl -u "${GITHUB_USERNAME}" -X POST -d "${DESC}" "https://api.github.com/gists"
+else
+  # ANONYMOUS GIST :
+  curl -X POST -d "${DESC}" "https://api.github.com/gists"
+fi
